e139abf89edd479e6f52c1f14227d8480cd36691
Rehaan11 <rameezrehaan@gmail.com>
2023-04-01 23:35:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Navbar from './Navbar'
+import Navbar from './alternative'
+function App() {
+  return (
+    <>
+    <Navbar/>
+
+    </>
+  );
+}
+
+export default App;
